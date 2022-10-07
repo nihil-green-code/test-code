4bd36b518e627f5608d1c5e39b5e1b186c3d45ab
NIHILncunia <nihil_ncunia@naver.com>
2022-10-07 18:13:22 +0900

    2022 1007

--- ex4/ex4.js
+++ ex4/ex4.js
@@ -21,37 +21,99 @@
 
 'use strict';
 
-let stName = prompt('학생명: ');
-// 이름 받음.
-let sum = 0;
-// 점수를 더할 변수 준비.
+// 합계 평균 따로 배치
+// let stName = prompt('학생명: ');
+// let sum = 0;
 
-const subjects = ['국어점수', '영어점수', '수학점수', '과학점수', '합계', '평균'];
-// 과목 배열. 과목을 추가하기만 하면 프로그램은 알아서 맞게 동작함.
-const scores = new Array(subjects.length);
-// 점수 배열 과목배열과 같은 공간으로 만들어야 하므로 이렇게 해줌.
+// const subjects = ['국어점수', '영어점수', '수학점수'];
+// const scores = new Array(subjects.length);
 
-// 입력을 받아서 점수 넣는 것과 점수 합계, 평균을 여기서 다 구함.
-for (let i = 0; i < subjects.length; i++) {
-  if (i < subjects.length - 2) {
-    scores[i] = +prompt(`${subjects[i]}`);
-    sum += scores[i];
-  } else if (i < subjects.length - 1) {
-    scores[i] = sum;
-  } else {
-    scores[i] = Math.trunc(sum / (subjects.length - 2));
+// for (let i = 0; i < subjects.length; i++) {
+//   scores[i] = +prompt(`${subjects[i]}`);
+//   sum += scores[i];
+// }
+
+// document.write('<table>');
+// document.write(`<caption>&lt; ${stName} 학생의 성적 &gt;</caption>`);
+// for (let i = 0; i < subjects.length; i++) {
+//   document.write('<tr>');
+//   document.write(`<th scope="col">${subjects[i]}</th>`);
+//   document.write(`<td>${scores[i]} 점</td>`);
+//   document.write('</tr>');
+// }
+
+// document.write(`<tr><th>합계</th>`);
+// document.write(`<td>${sum} 점</td></tr>`);
+
+// document.write(`<tr><th>평균</th>`);
+// document.write(`<td>${Math.trunc(sum / subjects.length)} 점</td></tr>`);
+
+// document.write('</table>');
+
+// 합계 평균 같이 구함
+// const subjects = ['국어점수', '영어점수', '수학점수', '합계', '평균'];
+// const scores = new Array(subjects.length);
+
+// for (let i = 0; i < subjects.length; i++) {
+//   if (i < subjects.length - 2) {
+//     scores[i] = +prompt(`${subjects[i]}`);
+//     sum += scores[i];
+//   } else if (i < subjects.length - 1) {
+//     scores[i] = sum;
+//   } else {
+//     scores[i] = Math.trunc(sum / (subjects.length - 2));
+//   }
+// }
+
+// document.write('<table>');
+// document.write(`<caption>&lt; ${stName} 학생의 성적 &gt;</caption>`);
+// for (let i = 0; i < subjects.length; i++) {
+//   document.write('<tr>');
+//   document.write(`<th scope="col">${subjects[i]}</th>`);
+//   document.write(`<td>${scores[i]} 점</td>`);
+//   document.write('</tr>');
+// }
+
+// document.write('</table>');
+
+// 2차원 배열 이용
+/*
+  [
+    ['학생명', '국어점수', '영어점수', '수학점수', '총점', '평균'],
+    ['김태현', 60, 80, 70, 210, 70],
+  ]
+*/
+
+let stNum = +prompt('학생 수');
+const stData = new Array(stNum + 1);
+stData[0] = ['학생명', '국어점수', '영어점수', '수학점수', '총점', '평균',];
+
+for (let row = 1; row < stData.length; row++) {
+  stData[row] = new Array(stData[0].length);
+  stData[row][stData[row].length - 2] = 0;
+  stData[row][stData[row].length - 1] = 0;
+  stData[row][0] = prompt(stData[0][0]);
+
+  for (let col = 1, sum, average; col < stData[0].length - 2; col++) {
+    stData[row][col] = +prompt(stData[0][col]);
+    sum = stData[row].length - 2;
+    average = stData[row].length - 1;
+
+    if (col > 0 && col < sum) stData[row][sum] += +stData[row][col];
+    if (col > 0 && col < average) stData[row][average] = Math.trunc(stData[row][sum] / (stData[row].length - 3));
   }
+
+  document.write('<table>');
+  document.write(`<caption>&lt; ${stData[row][0]} 학생의 성적 &gt;</caption>`);
+
+  for (let i = 1; i < stData[0].length; i++) {
+    document.write('<tr>');
+    document.write(`<th scope="col">${stData[0][i]}</th>`);
+    document.write(`<td>${stData[row][i]} 점</td>`);
+    document.write('</tr>');
+  }
+
+  document.write('</table>');
 }
 
-// 여기서 출력.
-document.write('<table>');
-document.write(`<caption>&lt; ${stName} 학생의 성적 &gt;</caption>`);
-for (let i = 0, content; i < subjects.length; i++) {
-  console.log(subjects.length, i);
-  document.write('<tr>');
-  // 과목 배열과 점수 배열을 이용해 각 칸을 만듬.
-  document.write(`<th scope="col">${subjects[i]}</th>`);
-  document.write(`<td>${scores[i]} 점</td>`);
-  document.write('</tr>');
-}
-document.write('</table>');
+console.log(stData);
