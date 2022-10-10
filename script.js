022191bb039e2ee3fee9e0026b94630871e77067
NIHILncunia <nihil_ncunia@naver.com>
2022-10-10 23:15:35 +0900

    2022 1010 update

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-/*
-  < 문제 >
+const person1 = {
+  name: 'NIHILncunia',
+};
 
-- 이름을 입력받아 대화상자 형태로 화면에 이름을 표시하는 프로그램을 구현.
-  단, 입력 대화상자가 비어있는 상태로 "확인" 버튼을 누르거나 "취소" 버튼을
-  눌렀을 때 "이름이 정상적으로 입력되지 않았습니다!!" 문구와 함께 페이지가
-  종료되도록 구현.
-*/
+const person2 = {
+  name: 'NIHILncunia',
+};
 
-let userName = prompt('이름 입력');
-
-if (userName) {
-  alert(userName);
-} else {
-  alert('이름이 정상적으로 입력되지 않았습니다.');
-  close();
-}
+console.log(person1 === person2); // 1번
+console.log(person1.name === person2.name); // 2번
---
+++ test.ts
@@ -0,0 +1,7 @@
+const obj = {};
+
+obj.string = 'nihilncunia';
+console.log(obj);
+
+obj.age = 30;
+console.log(obj);
